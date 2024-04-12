2a95f448e46392968e6ff78396601f0ff5feb922
ice-eca <ice_eca@mail.ru>
2024-04-12 16:32:41 +0500

    bug fix

--- main.py
+++ main.py
@@ -9,7 +9,6 @@ bot = telebot.TeleBot(TOKEN)
 phone_number_regex = re.compile(r'^(\+7|8)\d{10}$')
 age_regex = re.compile(r'^\d.*')
 district_regex = re.compile(r'^\D.*')
-city_regex = re.compile(r'^\D.*')
 data = {}
 request_chat_id = '-4136722281'
 
@@ -19,46 +18,25 @@ def enter_district(message):
     clear_data(message)
     data[message.chat.id] = {'stage':0}
     markup = types.InlineKeyboardMarkup()
-    itembtn1 = types.InlineKeyboardButton(text='Голева, 12', callback_data='Голева, 12')
-    itembtn2 = types.InlineKeyboardButton(text='1-я Красноармейская, 3', callback_data='Первая Красноармейская, 3')
+    itembtn1 = types.InlineKeyboardButton(text='Центр', callback_data='Центр')
+    itembtn2 = types.InlineKeyboardButton(text='ВИЗ', callback_data='ВИЗ')
+    itembtn3 = types.InlineKeyboardButton(text='Ботаника', callback_data='Ботаника')
+    itembtn4 = types.InlineKeyboardButton(text='Академический', callback_data='Академический')
     
-    markup.add(itembtn1, itembtn2)
+    markup.add(itembtn1, itembtn2, itembtn3, itembtn4)
     bot.send_photo(message.chat.id, open('kiber1.png', 'rb'))
-    bot.send_message(message.chat.id, 'Школа программирования для детей KIBERone ',reply_markup=markup)
+    bot.send_message(message.chat.id, 'Школа программирования для детей KIBERone Екатеринбург приветствует вас!\U0001F60A\n \nДля летних КИБЕРканикул мы подготовили для Вас 6 уникальных модулей, которые разработали на основе самых актуальных и трендовых тенденций IT-образования\U0001F4BB\n \n\U00002705Minecraft и таинственный портал (8+);\n \n\U00002705Создай свою карту в Roblox Studio (8+);\n \n\U00002705Новый Марио на Construct (8+);\n \n\U00002705Магия нейросетей. Курс искусственного интеллекта в помощь ребенку (8+);\n \n\U00002705Делаем супер-сайты со спецэффектами. Анимация в Figma (11+);\n \n\U00002705Создание интерактивной игры с собственном персонажем на конструкторе ClickTeam Fusion (11+).\n \nВыберите удобный для Вас район\U0001F447' , reply_markup=markup)
     
 def enter_age(message):
     markup = types.InlineKeyboardMarkup()
-    itembtn1 = types.InlineKeyboardButton(text='6-8 лет', callback_data='6-8')
-    itembtn2 = types.InlineKeyboardButton(text='9-11 лет', callback_data='9-11')
-    itembtn3 = types.InlineKeyboardButton(text='12-14 лет', callback_data='12-14')
-    markup.add(itembtn1, itembtn2, itembtn3)
+    itembtn1 = types.InlineKeyboardButton(text='8-10 лет', callback_data='8-10')
+    itembtn2 = types.InlineKeyboardButton(text='11-14 лет', callback_data='11-14')
+    markup.add(itembtn1, itembtn2)
     bot.send_message(message.chat.id, 'Пожалуйста, укажите возраст вашего ребенка\U0001F447',reply_markup=markup)
 
-def enter_city(message):
-    bot.send_message(message.chat.id, 'город')
-    
-@bot.message_handler(content_types=['text'])
-def handle_text(message):
-    if message.text.startswith('/start'):
-        return
-    if message.text.startswith('/getID'):
-        bot.send_message(message.chat.id, message.chat.id)
-        return
-    if message.text.startswith('/'):
-        bot.send_message(message.chat.id, 'Неверная команда')
-        return
-    if city_regex.match(message.text) and data[message.chat.id]['stage'] == 2:
-        data[message.chat.id]['phone_number'] = message.text
-        data[message.chat.id]['stage'] = 3
-        check_and_send(message)
-        return
-    else:
-        bot.send_message(message.chat.id, 'Повторите попытку')
-        return
 
 def enter_phone_number(message):
     bot.send_message(message.chat.id, 'Спасибо! Остался последний шаг\U0001F60A\n \nПожалуйста, введите номер телефона, по которому мы можем с Вами связаться\U0001F4F1')
-
     
 @bot.message_handler(content_types=['text'])
 def handle_text(message):
@@ -81,8 +59,8 @@ def handle_text(message):
 
 def check_and_send(message):
     if district_regex.match(data[message.chat.id]['district']) and age_regex.match(data[message.chat.id]['age']):
-        bot.send_message(message.chat.id, 'Спасибо! Скоро с вами свяжется наш администратор, отправит вам расписание мастер-классов на ближайшую неделю и согласует точное время\n \nДо встречи на уроке!\U0001F60A')
-        bot.send_message(request_chat_id, 'Адрес ' + data[message.chat.id]['district']+' возраст '+data[message.chat.id]['age']+' '+data[message.chat.id]['phone_number']+'город'+data[message.chat.id]['city'])
+        bot.send_message(message.chat.id, 'Спасибо! Скоро с Вами свяжется наш менеджер и подберёт подходящую смену для Вашего ребёнка. \n \nДо встречи на летних КИБЕРканикулах!\U0001F60A')
+        bot.send_message(request_chat_id, '\U00002757\U00002757\U00002757 Новый лид\U00002757\U00002757\U00002757'+'\nРайон: ' + data[message.chat.id]['district']+'\nВозраст: '+data[message.chat.id]['age']+'\nТел: '+data[message.chat.id]['phone_number'])
         clear_data(message)
     else:
         bot.send_message(message.chat.id, 'Неправильно сформированы ответы на вопросы, поробуйте еще раз')
